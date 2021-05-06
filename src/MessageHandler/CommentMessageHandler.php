b5d867ccd87448eeb40127ab3fec86fb382451ac
k01yasik <bzdykin@mail.ru>
2021-05-06 16:36:07 +0300

    After chapter 19

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -8,14 +8,20 @@ use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Workflow\WorkflowInterface;
 
 class CommentMessageHandler implements MessageHandlerInterface
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
         private SpamChecker $spamChecker,
-        private CommentRepository $commentRepository
+        private CommentRepository $commentRepository,
+        private MessageBusInterface $bus,
+        private WorkflowInterface $commentStateMachine,
+        private ?LoggerInterface $logger = null
     )
     {
     }
@@ -26,10 +32,32 @@ class CommentMessageHandler implements MessageHandlerInterface
 
         if (!$comment) return;
 
-        if (2 === $this->spamChecker->getSpamScore($comment, $message->getContext())) {
-            $comment->setState('spam');
-        } else {
-            $comment->setState('published');
+        if ($this->commentStateMachine->can($comment, 'accept')) {
+            $score = $this->spamChecker->getSpamScore($comment, $message->getContext());
+            $transition = 'accept';
+
+            if (2 === $score) {
+                $transition = 'reject_spam';
+            } elseif (1 === $score) {
+                $transition = 'might_be_spam';
+            }
+            $this->commentStateMachine->apply($comment, $transition);
+            $this->entityManager->flush();
+            $this->bus->dispatch($message);
+        } elseif ($this->commentStateMachine->can($comment, 'publish') || $this->commentStateMachine->can($comment, 'publish_ham')) {
+            $this->commentStateMachine->apply(
+                $comment,
+                $this->commentStateMachine->can(
+                    $comment, 'publish'
+                ) ? 'publish': 'publish_ham');
+            $this->entityManager->flush();
+        } elseif ($this->logger) {
+            $this->logger->debug('Dropping comment message',
+                [
+                    'comment' => $comment->getId(),
+                    'state' => $comment->getState()
+                ]
+            );
         }
 
         $this->entityManager->flush();
