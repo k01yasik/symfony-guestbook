32bef1d8a851ac6c902659bddf37cf26936ca6af
k01yasik <bzdykin@mail.ru>
2021-05-10 01:38:54 +0300

    After chapter 22

--- assets/app.js
+++ assets/app.js
@@ -5,8 +5,13 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+import 'bootstrap';
+import bsCustomFileInput from "bs-custom-file-input";
+
+bsCustomFileInput.init();
